425db2fd0e86b4b9ab42f7c0e56150f1291ecbff
jnchiarot <natalina.chiarot@keyin.com>
2024-02-14 03:01:15 -0330

    FS logic applied to all routes.

--- index.js
+++ index.js
@@ -25,21 +25,51 @@ const server = http.createServer((request, response) => {
       console.log("Root page rendered successfully.");
       break;
     case "/about":
+      fs.readFile("./views/about.html", function (err, data) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(data);
+        return response.end();
+      });
       console.log("About page rendered successfully.");
       break;
     case "/contact":
+      fs.readFile("./views/contact.html", function (err, data) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(data);
+        return response.end();
+      });
       console.log("Contact page rendered successfully.");
       break;
     case "/products":
+      fs.readFile("./views/products.html", function (err, data) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(data);
+        return response.end();
+      });
       console.log("Products page rendered successfully.");
       break;
     case "/subscribe":
+      fs.readFile("./views/subscribe.html", function (err, data) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(data);
+        return response.end();
+      });
       console.log("Subscribe page rendered successfully.");
       break;
     case "/updates":
+      fs.readFile("./views/updates.html", function (err, data) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(data);
+        return response.end();
+      });
       console.log("Updates page rendered successfully.");
       break;
     case "/promotions":
+      fs.readFile("./views/promotions.html", function (err, data) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(data);
+        return response.end();
+      });
       console.log("Promotions page rendered successfully.");
       break;
     default:
