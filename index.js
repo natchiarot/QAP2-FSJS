96fe9a3a5b404443fb976b60c71d424c94279249
jnchiarot <natalina.chiarot@keyin.com>
2024-02-14 02:57:39 -0330

    Successfully reading and displaying html files from views folder.

--- index.js
+++ index.js
@@ -5,10 +5,23 @@ const port = 3003;
 
 global.DEBUG = true;
 
+const fs = require("fs");
+
 const server = http.createServer((request, response) => {
+  if (request.url === "/favicon.ico") {
+    // Handling favicon.ico requests - so they're ignored.
+    response.writeHead(204, { "Content-Type": "image/x-icon" });
+    response.end();
+    return;
+  }
   if (DEBUG) console.log(`Request url: ${request.url}`);
   switch (request.url) {
     case "/":
+      fs.readFile("./views/home.html", function (err, data) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(data);
+        return response.end();
+      });
       console.log("Root page rendered successfully.");
       break;
     case "/about":
