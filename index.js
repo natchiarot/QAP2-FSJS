623c987f02e64e2b1d5dc61e648bfa3150910416
jnchiarot <natalina.chiarot@keyin.com>
2024-02-14 04:19:19 -0330

    Cleaned up file reading and added event emitters.

--- index.js
+++ index.js
@@ -17,59 +17,73 @@ const server = http.createServer((request, response) => {
   if (DEBUG) console.log(`Request url: ${request.url}`);
   switch (request.url) {
     case "/":
-      fs.readFile("./views/home.html", function (data) {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(data);
-        return response.end();
-      });
+      myEmitter.emit(
+        "event",
+        request.url,
+        "INFO",
+        "Root page rendered successfully."
+      );
+      serverPage("./views/home.html", response);
       console.log("Root page rendered successfully.");
       break;
     case "/about":
-      fs.readFile("./views/about.html", function (data) {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(data);
-        return response.end();
-      });
+      myEmitter.emit(
+        "event",
+        request.url,
+        "INFO",
+        "About page rendered successfully."
+      );
+      serverPage("./views/about.html", response);
       console.log("About page rendered successfully.");
       break;
     case "/contact":
-      fs.readFile("./views/contact.html", function (data) {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(data);
-        return response.end();
-      });
+      myEmitter.emit(
+        "event",
+        request.url,
+        "INFO",
+        "Contact page rendered successfully."
+      );
+      serverPage("./views/contact.html", response);
       console.log("Contact page rendered successfully.");
       break;
     case "/products":
-      fs.readFile("./views/products.html", function (data) {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(data);
-        return response.end();
-      });
+      myEmitter.emit(
+        "event",
+        request.url,
+        "INFO",
+        "Contact page rendered successfully."
+      );
+      serverPage("./views/contact.html", response);
       console.log("Products page rendered successfully.");
       break;
     case "/subscribe":
-      fs.readFile("./views/subscribe.html", function (data) {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(data);
-        return response.end();
-      });
+      myEmitter.emit(
+        "event",
+        request.url,
+        "INFO",
+        "Subscribe page rendered successfully."
+      );
+      serverPage("./views/subscribe.html", response);
       console.log("Subscribe page rendered successfully.");
       break;
     case "/updates":
-      fs.readFile("./views/updates.html", function (data) {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(data);
-        return response.end();
-      });
+      myEmitter.emit(
+        "event",
+        request.url,
+        "INFO",
+        "Updates page rendered successfully."
+      );
+      serverPage("./views/updates.html", response);
       console.log("Updates page rendered successfully.");
       break;
     case "/promotions":
-      fs.readFile("./views/promotions.html", function (data) {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(data);
-        return response.end();
-      });
+      myEmitter.emit(
+        "event",
+        request.url,
+        "INFO",
+        "Promotions page rendered successfully."
+      );
+      serverPage("./views/promotions.html", response);
       console.log("Promotions page rendered successfully.");
       break;
     default:
@@ -86,3 +100,17 @@ const server = http.createServer((request, response) => {
 server.listen(port, () => {
   console.log(`The server is running on port ${port}`);
 });
+
+function serverPage(filePath, response) {
+  fs.readFile(filePath, function (err, data) {
+    if (err) {
+      myEmitter.emit("ERROR", err);
+      response.writeHead(500, { "Content-Type": "text/plain" });
+      response.write(`500 Internal Server Error`);
+      return response.end();
+    }
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(data);
+    response.end();
+  });
+}
